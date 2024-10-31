cd49761de6a894c52a4d478f881ffc181073a39a
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-31 19:43:56 +0000

    Del pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -30,9 +30,9 @@ class Server:
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            data = self.dataset()
+            truncated_dataset = data[:1000]
+            self.__indexed_dataset = {i: data[i] for i in range(len(data))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
