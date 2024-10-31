a4cdb28a6ab3da4643f374a5f5eebacf9a528b8f
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-31 19:30:09 +0000

    Del pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -9,8 +9,8 @@ from typing import List, Dict, Any
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,31 +28,28 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            """
-            Get hyper
-            """
-            data = []
-            current_index = index
-            indexed_keys = list(self.__indexed_dataset.keys())
-            while len(data) < page_size and current_index < len(indexed_keys):
-                if current_index in self.__indexed_dataset:
-                    data.append(self.__indexed_dataset[current_index])
-                current_index += 1
-            next_index = current_index if current_index < len(indexed_keys) else None
-            return {
-                    "index": index,
-                    "next_index": next_index,
-                    "page_size": len(data),
-                    "data": data
-                    }
+        """
+        Get hyper
+        """
+        data = []
+        current_index = index
+        indexed_keys = list(self.__indexed_dataset.keys())
+        while len(data) < page_size and current_index < len(indexed_keys):
+            if current_index in self.__indexed_dataset:
+                data.append(self.__indexed_dataset[current_index])
+            current_index += 1
+        next_index = current_index if current_index < len(indexed_keys) else None
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data,
+        }
