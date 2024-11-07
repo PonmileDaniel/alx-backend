f4f98907ea17f2ec5e871037f2ebc816c0cd172f
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-11-07 01:19:09 +0000

    Basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,13 +6,17 @@ from flask_babel import Babel
 
 
 class Config:
-    LANGUAGE = ["en", "fr"]
+    '''Config class '''
+
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
 
