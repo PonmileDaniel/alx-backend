57a9e2a896e3420c389c6667bd15cc32a53ba37e
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-07 23:44:22 +0100

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Get locale form request"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config Class"""
+     
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """Default routes"""
+    return render_template("2-index.html")
+
+
+@babel.localselector
+def get_locale():
+    return request.accept_language.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
