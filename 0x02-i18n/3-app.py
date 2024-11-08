705b25cd59e4020e1dbd0bdd5dbca2abfa025ebd
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-08 17:36:09 +0100

    Documentation error

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,19 +8,16 @@ from flask_babel import Babel, _
 
 
 class Config(object):
-    """ Config Class """
+    """Config Class"""
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Instantiate the app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-
-# wrap the app with babel
 babel = Babel(app)
 
 
@@ -35,7 +32,10 @@ def get_locale() -> str:
 
 @app.route("/", strict_slashes=False)
 def index() -> str:
-    """ Default route for the html page """
+    """ Default route for the html page
+     
+    Returns:
+        str: home page """
     return render_template("3-index.html")
 
 
