7d7f605f9ee81905323c97aa0c92ec712cc6b51b
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-08 13:03:01 +0100

    checker error

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,31 +8,36 @@ from flask_babel import Babel, _
 
 
 class Config(object):
-    """ Config Class
+    """
+    Config Class
     """
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
+# Instantiate the app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 
+# wrap the app with babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """ Get the best match
+    """
+    Get the best match
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/", strict_slashes=False)
 def index() -> str:
-    """Default route"""
+    """
+    Default route
+    """
     return render_template("3-index.html")
 
 
